10f09b8b8b35833c00b17ca8667818ea7bdfe635
lucid_dreamer <impera.et.conquer@gmail.com>
2019-12-15 22:57:10 +0200

    test

--- src/sample/sample/Main.java
+++ src/sample/sample/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
-		//test
+		//tests
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
